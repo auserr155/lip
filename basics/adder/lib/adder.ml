0334d7601f0ca82a43845381a7388407c03eadf4
Docker <docker@example.com>
2024-10-09 14:20:24 +0100

    More accurate comments

--- basics/adder/lib/adder.ml
+++ basics/adder/lib/adder.ml
@@ -3,5 +3,7 @@
 
 let rec addlist l =
   match l with
+  (* If the list is empty ([]), it returns 0.*)
   | [] -> 0
+  (* Otherwise, it adds the head of the list to the result of recursively calling addlist on the tail of the list.*)
   | head :: tail -> head + addlist tail
